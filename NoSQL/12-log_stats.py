3971f9a7715a65323e9eba979ac21203a0ea83d1
Louveau Clément <157509225+valak74200@users.noreply.github.com>
2025-05-01 12:10:48 +0200

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,26 +1,25 @@
-#!/usr/bin/env python3
-"""
-Write a Python script that provides some stats about
-Nginx logs stored in MongoDB
-"""
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    """
-    Database: logs
-    Collection: nginx
-    """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+def log_stats():
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
 
-    n_logs = nginx_collection.count_documents({})
-    print(f'{n_logs} logs')
+    # Total number of logs
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Methods stats
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
+    print("Methods:")
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f'{status_check} status check')
+    # GET requests to /status
+    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
+
+if __name__ == "__main__":
+    log_stats()
+

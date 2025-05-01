4eef76d040786ea254de9b1dac3bfa79ab4ab396
Louveau Clément <157509225+valak74200@users.noreply.github.com>
2025-05-01 11:57:37 +0200

    Create 0-simple_helper_function.py

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+
+
+def index_range(page, page_size):
+    """Returns a tuple of size two containing a start index and an end index"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)

9c6e773f6776c9a088e5c29b88b97487b9c81aaa
valak74200 <clmentlv@outlook.fr>
2025-05-21 15:30:27 +0200

    nex config

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (d) => {
-  if (d.toString().trim() !== '') {
-    process.stdout.write(`Your name is: ${d.toString()}`);
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  if (name !== '') {
+    process.stdout.write(`Your name is: ${name}\n`);
   } else {
     process.exit();
   }
 });
+
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
   process.exit();

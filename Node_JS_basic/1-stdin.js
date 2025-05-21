11dfcdbe9dc122f01889e4c27a2e004a6d177d2a
valak74200 <clmentlv@outlook.fr>
2025-05-21 15:21:34 +0200

    Refactor stdin data handling to improve name input processing

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (d) => {
-  if (d.toString().trim() !== '') {
-    process.stdout.write(`Your name is: ${d.toString()}`);
-  } else {
-    process.exit();
-  }
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
 });
+
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit();
 });

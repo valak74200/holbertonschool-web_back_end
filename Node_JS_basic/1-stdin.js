200eccf68bcb3772c596e74901ce473450d2901f
valak74200 <clmentlv@outlook.fr>
2025-05-21 15:32:12 +0200

    new

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,11 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  if (name !== '') {
-    process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('data', (d) => {
+  if (d.toString().trim() !== '') {
+    process.stdout.write(`Your name is: ${d.toString()}`);
   } else {
     process.exit();
   }
 });
-
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
   process.exit();
